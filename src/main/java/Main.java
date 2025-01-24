bf56dedbe696e81c334fb71a1e89dadbe6cbf83e
HightechTR <explosion64.hightechtr@gmail.com>
2025-01-24 17:42:24 +0800

    Revert "update"
    This reverts commit 56c985db21f81938fcee3c627901900784bc46b2.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,29 @@
 public class Main {
-    static Loan[] loans = new Loan[100];
+    static int[] amounts = new int[100];
+    static String[] borrowers = new String[100];
+    static boolean[] isPaid = new boolean[100];
+    static double[] interestRates = new double[100];
     static int loansCount = 0;
 
-    public static void addLoan(String borrower, int amount, double interestRate, String contactNumber) {
-        loans[loansCount] = new Loan(amount, borrower, interestRate, contactNumber);
+    public static void addLoan(String borrower, int amount, double interestRate) {
+        borrowers[loansCount] = borrower;
+        amounts[loansCount] = amount;
+        interestRates[loansCount] = interestRate;
         loansCount++;
     }
 
     public static void setAsPaid(String borrower) {
         for (int i = 0; i < loansCount; i++) {
-            if (loans[i].getBorrower().equals(borrower)) {
-                loans[i].setPaid(true);
+            if (borrowers[i].equals(borrower)) {
+                isPaid[i] = true;
             }
         }
     }
 
     public static void listLoans() {
         for (int i = 0; i < loansCount; i++) {
-            if (!loans[i].isPaid()) {
-                System.out.println(loans[i].getBorrower() + ": " + loans[i].getAmount());
+            if (!isPaid[i]) {
+                System.out.println(borrowers[i] + ": " + amounts[i]);
             }
         }
     }
@@ -27,21 +32,19 @@ public class Main {
         double profit = 0;
         for (int i = 0; i < loansCount; i++) {
             // if the loan was repaid, add the interest to profit
-            if (loans[i].isPaid()) {
-                profit = profit + (loans[i].getAmount() * loans[i].getInterestRate());
+            if (isPaid[i]) {
+                profit = profit + (amounts[i] * interestRates[i]);
             }
         }
         return profit;
     }
 
     public static void main(String[] args) {
-        addLoan("Alice Betsy", 10, 0.1, "12345678");
-        addLoan("Ben Chee", 20, 0.2, "87654312");
-        addLoan("Chris Davis", 30, 0.3, "42069696");
+        addLoan("Alice Betsy", 10, 0.1);
+        addLoan("Ben Chee", 20, 0.2);
+        addLoan("Chris Davis", 30, 0.3);
         setAsPaid("Ben Chee");
         listLoans();
         System.out.println("profit: " + calculateProfitEarned());
-
-
     }
 }

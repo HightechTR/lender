56c985db21f81938fcee3c627901900784bc46b2
HightechTR <explosion64.hightechtr@gmail.com>
2025-01-24 17:40:32 +0800

    update

---
+++ src/main/java/Loan.java
@@ -0,0 +1,55 @@
+public class Loan {
+    private int amount;
+    private String borrower;
+    private boolean isPaid;
+    private double interestRate;
+    private String contactNumber;
+    static int loansCount = 0;
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Loan(int amount, String borrower, double interestRate, String contactNumber) {
+        this.amount = amount;
+        this.borrower = borrower;
+        this.interestRate = interestRate;
+        this.contactNumber = contactNumber;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,24 @@
 public class Main {
-    static int[] amounts = new int[100];
-    static String[] borrowers = new String[100];
-    static boolean[] isPaid = new boolean[100];
-    static double[] interestRates = new double[100];
+    static Loan[] loans = new Loan[100];
     static int loansCount = 0;
 
-    public static void addLoan(String borrower, int amount, double interestRate) {
-        borrowers[loansCount] = borrower;
-        amounts[loansCount] = amount;
-        interestRates[loansCount] = interestRate;
+    public static void addLoan(String borrower, int amount, double interestRate, String contactNumber) {
+        loans[loansCount] = new Loan(amount, borrower, interestRate, contactNumber);
         loansCount++;
     }
 
     public static void setAsPaid(String borrower) {
         for (int i = 0; i < loansCount; i++) {
-            if (borrowers[i].equals(borrower)) {
-                isPaid[i] = true;
+            if (loans[i].getBorrower().equals(borrower)) {
+                loans[i].setPaid(true);
             }
         }
     }
 
     public static void listLoans() {
         for (int i = 0; i < loansCount; i++) {
-            if (!isPaid[i]) {
-                System.out.println(borrowers[i] + ": " + amounts[i]);
+            if (!loans[i].isPaid()) {
+                System.out.println(loans[i].getBorrower() + ": " + loans[i].getAmount());
             }
         }
     }
@@ -32,19 +27,21 @@ public class Main {
         double profit = 0;
         for (int i = 0; i < loansCount; i++) {
             // if the loan was repaid, add the interest to profit
-            if (isPaid[i]) {
-                profit = profit + (amounts[i] * interestRates[i]);
+            if (loans[i].isPaid()) {
+                profit = profit + (loans[i].getAmount() * loans[i].getInterestRate());
             }
         }
         return profit;
     }
 
     public static void main(String[] args) {
-        addLoan("Alice Betsy", 10, 0.1);
-        addLoan("Ben Chee", 20, 0.2);
-        addLoan("Chris Davis", 30, 0.3);
+        addLoan("Alice Betsy", 10, 0.1, "12345678");
+        addLoan("Ben Chee", 20, 0.2, "87654312");
+        addLoan("Chris Davis", 30, 0.3, "42069696");
         setAsPaid("Ben Chee");
         listLoans();
         System.out.println("profit: " + calculateProfitEarned());
+
+
     }
 }
